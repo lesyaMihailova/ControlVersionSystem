14685c1951d223ea1dea42d9a6508ddb6a7de103
Lesya <Lesya_mixailova@mail.ru>
2013-02-25 19:38:40 +0400

    +

---
+++ ControlVersionSystem/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WpfApplication1
+{
+     
+    public static class Tools
+    {
+        public static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+    }
+}
---
+++ ControlVersionSystem/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    public class VersionText
+    {
+        public string VersName { get; set; }
+        public string VersText { get; set; }
+    }
+}
